5abc3d3f10f8529fdbf9791702394c8c5a02e55d
¨Brunovbf¨ <bvbf20@gmail.com>
2023-09-05 15:06:22 -0300

    Comentarios

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ const nombreInput = document.getElementById("nombre");
 const apellidoInput = document.getElementById("apellido");
 const fechaNacimientoInput = document.getElementById("fechaNacimiento");
 
-//agregamos una escucha al evento submit
+//Agregamos un evento de escucha al formulario cuando se envia
 document.getElementById("registroForm").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    //obtienemos las variables definidas anteriormente
+    //Obtenemos los valores de los campos que el usuario ingresa
     const nombre = nombreInput.value;
     const apellido = apellidoInput.value;
     const fechaNac = fechaNacimientoInput.value;
-
+    
+    //Creamos un objeto con los datos
     const dataUsuario = {
         nombre: nombre,
         apellido: apellido,
         fechaNac: fechaNac,
 
     };
+
+    //Enviamos los datos al servidor en formato json
     fetch("https://jsonplaceholder.typicode.com/users", {
         method: 'POST',
         data: JSON.stringify(dataUsuario)
